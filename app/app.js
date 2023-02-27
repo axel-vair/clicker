9612fd66cc83a2d7e61236a71e5c7ccdcaf7103c
axelvair <axel.vair@laplateforme.io>
2023-02-27 17:52:04 +0100

    incrementation, localstorage, auto boost

---
+++ app/app.js
@@ -0,0 +1,83 @@
+/* DÉCLARATIONS DOM */
+const body = document.querySelector("body");
+const clickerSection = document.querySelector(".clicker-section");
+const shopSection = document.querySelector(".shop-section");
+const clickerContainer = document.querySelector(".clicker-container")
+const btnSemiAutoBuy = document.querySelector("#semi-auto-buy");
+const btnAutoBuy = document.querySelector("#auto-buy");
+const btnMultipleBuy = document.querySelector("#multiple-buy");
+const btnMultipleDeuxBuy = document.querySelector("#multiple-deux-buy");
+
+/* BUTTON CLICKER */
+const btnClicker = document.createElement("button");
+btnClicker.classList.add("btnClicker");
+btnClicker.innerHTML = `Clicker ${localStorage.getItem('countStock')}`;
+clickerContainer.appendChild(btnClicker);
+
+
+/* LISTENER CLICKER */
+btnClicker.addEventListener("click", () => {
+    countClick();
+})
+
+/* LISTENER SHOP */
+    /* ~~ semi auto ~~ */
+btnSemiAutoBuy.addEventListener("click", () => {
+    console.log("btnSemiAuto")
+})
+
+    /* ~~ auto ~~ */
+btnAutoBuy.addEventListener("click", () => {
+    autoBuy();
+})
+
+    /* ~~ multiple  ~~ */
+btnMultipleBuy.addEventListener("click", () => {
+    console.log("multiple")
+})
+
+    /* ~~multiple 2 ~~ */
+btnMultipleDeuxBuy.addEventListener("click", () => {
+    multipleDeux();
+})
+
+
+function countClick(){
+    count++;
+    localStorage.setItem('countStock', JSON.stringify(count));
+    btnClicker.innerHTML = `Clicker ${localStorage.getItem('countStock')}`;
+}
+
+/* FUNCTIONS SHOP */
+
+    /* ~~ function semi auto ~~ */
+function semiAutoBuy(){
+    count = count * 0.1;
+}
+    /* ~~ function auto ~~ */
+function autoBuy(){
+    let interval = setInterval(() => {
+        localStorage.setItem("countStock",count++);
+        btnClicker.innerHTML = `Clicker ${localStorage.getItem('countStock')}`;
+    }, 1000);
+}
+    /* ~~ function multiple ~~ */
+function multipleBuy(){
+
+}
+    /* ~~ function multiple 2  ~~ */
+
+function multipleDeux(){
+    localStorage.setItem('keyValue', JSON.stringify(count+2));
+
+}
+
+
+
+/* SCORE */
+let count;
+if(localStorage.getItem('countStock') != null){
+    count = localStorage.getItem('countStock');
+}else{
+    count = 0;
+}
--- clicker.php
+++ clicker.php
@@ -1,1 +1,48 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="app/app.js" defer></script>
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Clicker Game</title>
+</head>
+<body>
+<h1>Clicker Game</h1>
+<section class="clicker-section">
+    <div><p id="score"></p> </div>
+    <div class="clicker-container"></div>
+</section>
+<section class="shop-section">
+    <div class="semi-auto shop">
+        <h2>Semi-auto</h2>
+        <p class="description-shop">La description de mon bonus sera ici</p>
+        <button id="semi-auto-buy">Acheter</button>
+    </div>
+    <div class="auto shop">
+        <h2>Auto</h2>
+        <p class="description-shop">La description de mon bonus sera ici</p>
+        <button id="auto-buy">Acheter 100 bites</button>
+
+    </div>
+    <div class="multiple shop">
+        <h2>Multiple x1.2</h2>
+        <p class="description-shop">La description de mon bonus sera ici</p>
+        <button id="multiple-buy">Acheter</button>
+
+    </div>
+    <div class="multiple-deux shop">
+        <h2>Multiple x2</h2>
+        <p class="description-shop">La description de mon bonus sera ici</p>
+        <button id="multiple-deux-buy">Acheter</button>
+
+    </div>
+</section>
+
+</body>
+</html>
